3b5fc119ae60dabd2290a88ab0b5b5b59a4837ca
张帆@Home <zhangfan@dajiagame.com>
2015-12-01 22:38:39 +0800

    增加了试做测试

---
+++ CheckGUID/test/TryTest.py
@@ -0,0 +1,19 @@
+import unittest
+
+
+def division_funtion(x, y):
+    return round(float(x) / y, 6)
+
+
+class TestDivision(unittest.TestCase):
+    def test_int(self):
+        self.assertEqual(division_funtion(9, 3), 3)
+
+    def test_int2(self):
+        self.assertEqual(division_funtion(9, 4), 2.25)
+
+    def test_float(self):
+        self.assertEqual(division_funtion(4.2, 3), 1.4)
+
+if __name__ == '__main__':
+    unittest.main()
