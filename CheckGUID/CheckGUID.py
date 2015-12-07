4d956d4e09fdce587a1f78a79a4b8455a6e37cc3
张帆 <zhangfan@dajiagame.com>
2015-12-07 15:00:24 +0800

    制作命令行版本的CheckGUID

--- CheckGUID/CheckGUID.py
+++ CheckGUID/CheckGUID.py
@@ -1,30 +1,38 @@
 import os
-from tkinter import *
-from tkinter import ttk
-from tkinter import filedialog
 
-#需求
-#检查指定文件夹下(包括子文件夹)所有文件的GUID
-#统计在目标文件夹内的引用次数，从少到多排序
-#结束显示里要包括受检查的文件名、引用次数、引用了的文件名
+# 命令行版工具需求
+# 1.等待输入搜索范围文件夹路径
+# 2.等待输入搜索文件路径
+# 3.列出所有结果
+# 4.回到2
 
-#UI
+guidTag = r'guid: '
+targetFolderPath = input('搜索范围')
 
+while True:
+    targetFile = input('搜索文件')
+    if targetFile == 'exit':
+        SystemExit()
 
-def ChooseFolder():
-    dirPath.set(filedialog.askdirectory())
+    #get GUID
+    guid = ''
+    metaPath = targetFile+'.meta'
+    lines = open(metaPath).readlines()
+    for line in lines:
+        if line.startswith(guidTag):
+            guid = line.lstrip(guidTag)
+            print(guid)
+            break
 
-#选择来源文件夹与目标文件夹
-root  = Tk()
-root.title("检查GUID")
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-dirPath = StringVar()
-ttk.Button(mainframe, text="选择源文件夹", command=ChooseFolder).grid(column=1, row=1, sticky=W)
-ttk.Label(mainframe, textvariable=dirPath).grid(column=2, row=1, sticky=E)
-
-root.mainloop()
+    # search
+    for folderName, subfolders, filenames in os.walk(targetFolderPath):
+        for filename in filenames:
+            fullPath = folderName+'\\'+filename
+            if os.path.isfile(fullPath) and (fullPath.endswith('.prefab') or fullPath.endswith('.unity')) and fullPath!=targetFile:
+                try:
+                    lines = open(fullPath).readlines()
+                    for line in lines:
+                        if guid in line:
+                            print(fullPath)
+                except:
+                    print(fullPath + ": ERROR")
